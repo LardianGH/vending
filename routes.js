56ed75b0afbb709db79c66be0348ee227e00f426
Zion Brown <zabs95@gmail.com>
2023-03-02 00:13:23 -0500

    just messed with the css a bit to make the view look like a vending machine, a few example sodas derrived from the routes.js file

--- routes.js
+++ routes.js
@@ -6,7 +6,16 @@ const router = express.Router();
 // });
 
  router.get('/', (req, res) => {
-  res.render('index', { name: 'World', title: 'Vending' });
+  res.render('index', { name: 'World', title: 'Vending', 
+  soda1: 'Coca-Cola',
+  soda2: 'Pepsi',
+  soda3: 'Sprite',
+  soda4: 'Fanta',
+  soda5: 'Dr. Pepper',
+  soda6: 'Mountain Dew',
+  soda7: '7UP',
+  soda8: 'A&W Root Beer'
+});
 }); 
 
   module.exports = router;
